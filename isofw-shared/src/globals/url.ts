286f7412baacc330fa3e6612e44c4755244d0e74
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-10 08:35:34 +0100

    :wrench: Prefix URLs with the/

--- isofw-shared/src/globals/url.ts
+++ isofw-shared/src/globals/url.ts
@@ -2,9 +2,9 @@ const urls = {
   webPrefix: `http://`,
   wsPrefix: `ws://`,
   mainServer: `localhost:4202`,
-  directorPage: "director",
-  programPage: "program",
-  projectOverview: "projectOverview"
+  directorPage: "/director",
+  programPage: "/program",
+  projectOverview: "/projectOverview"
 }
 
 export default urls
--- isofw-web/src/app.tsx
+++ isofw-web/src/app.tsx
@@ -32,9 +32,9 @@ const AppRouter = () => (
               <Route path="/create/:collection" component={LoadableCreatePage} key="create" />
               <Route path="/list/:collection" component={LoadableListPage} key="list" />
               <Route path="/edit/:collection/:id" component={LoadableEditPage} key="edit" />
-              <Route path={`/${urls.directorPage}/:id`} component={LoadableDirectorPage} key="director" />
-              <Route path={`/${urls.programPage}/:id`} component={LoadableProgramPage} key="program" />
-              <Route path={`/${urls.projectOverview}`} component={LoadableProjectOverview} key="project" />
+              <Route path={`${urls.directorPage}/:id`} component={LoadableDirectorPage} key="director" />
+              <Route path={`${urls.programPage}/:id`} component={LoadableProgramPage} key="program" />
+              <Route path={`${urls.projectOverview}`} component={LoadableProjectOverview} key="project" />
             </Switch>
           </RouteContainer>
         </PoseGroup>
