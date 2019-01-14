fcd343b774f583239ed79bc34963d4450fed562f
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-14 16:33:57 +0100

    :sparkles: Add icon to menu

--- isofw-web/src/components/menuPanel.tsx
+++ isofw-web/src/components/menuPanel.tsx
@@ -1,37 +1,52 @@
-import { Panel, View, Page, Block, List,Link, ListItem } from "framework7-react";
+import { Panel, View, Page, Block, List,Link, ListItem, Icon } from "framework7-react";
 import * as React from "react"
 import urls from "isofw-shared/src/globals/url"
+import { SharedFormAuth, IFormAuthProps } from "@xpfw/ui-shared"
 
-class MenuPanel extends React.Component<any, any> {
-  public render() {
-    console.log("THI SIS", this)
-    return (
-      <Panel left reveal>
-          <Page>
-            <Block>
-              Poly direct
-            </Block>
-            <List>
-              <ListItem
-                link="/"
-                title="Home"
-                panelClose="left"
-              />
+const MenuPanel: React.FunctionComponent<IFormAuthProps> = (props) => {
+  return (
+    <Panel left reveal>
+        <Page>
+          <Block>
+            Poly direct
+          </Block>
+          <List>
+            <ListItem
+              link="/"
+              title="Home"
+              panelClose="left"
+            >
+              <Icon slot="media" fa="home" />
+            </ListItem>
+            {props.loggedIn ? null : (
               <ListItem
                 link="/login"
                 title="Login"
                 panelClose="left"
-              />
+              >
+                <Icon slot="media" fa="sign-in-alt" />
+              </ListItem>
+            )}
+            <ListItem
+              link={urls.projectOverview}
+              title="Projects"
+              panelClose="left"
+            >
+              <Icon slot="media" fa="folder" />
+            </ListItem>
+            {!props.loggedIn ? null : (
               <ListItem
-                link={urls.projectOverview}
-                title="Projects"
+                link="/login"
+                title="Logout"
                 panelClose="left"
-              />
-            </List>
-          </Page>
-      </Panel>
-    )
-  }
+              >
+                <Icon slot="media" fa="sign-out-alt" />
+              </ListItem>
+            )}
+          </List>
+        </Page>
+    </Panel>
+  )
 }
 
-export default MenuPanel
+export default SharedFormAuth<{}>(MenuPanel)
