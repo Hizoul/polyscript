a6f0ce12af3edbd892ef8860798d940e2e8ded3f
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-16 17:17:49 +0100

    :sparkles: Make home useful

--- isofw-web/src/pages/home.tsx
+++ isofw-web/src/pages/home.tsx
@@ -1,9 +1,34 @@
 import * as React from "react"
 import WebPageContainer from "isofw-web/src/components/pageContainer";
+import { List, ListItem, Icon, BlockTitle } from "framework7-react";
+import urls from "isofw-shared/src/globals/url";
 
 const Home: React.FunctionComponent<any> = (props) => {
   return (
-    <WebPageContainer {...props} name="home" title="Polycast">Home</WebPageContainer>
+    <WebPageContainer {...props} name="home" title="Poly direct">
+      <BlockTitle>
+        welcome to poly direct.<br />
+        what do you wish to do?
+      </BlockTitle>
+      <List>
+        <ListItem
+          link={urls.projectOverview}
+          title="Projects"
+          key="Projects"
+          panelClose="left"
+        >
+          <Icon slot="media" fa="folder" />
+        </ListItem>
+        <ListItem
+          link={urls.cameraOverview}
+          title="Cameras"
+          key="Cameras"
+          panelClose="left"
+        >
+          <Icon slot="media" fa="camera-retro" />
+        </ListItem>
+      </List>
+    </WebPageContainer>
   )
 }
 
