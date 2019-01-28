1a775bc45fd78b54c9be47db461e2e29ecb971c6
Matthias Müller-Brockhausen <git@mmb2.click>
2019-01-28 14:27:58 +0100

    :sparkles: scrolling in native

--- isofwrn/src/App.tsx
+++ isofwrn/src/App.tsx
@@ -4,25 +4,54 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View, Image
-} from 'react-native';
-import SharedImage from "isofw-shared/src/sharedComponentWithImageImport"
+  View, Image, FlatList
+} from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+  android: 'Double tap R on your keyboard tho reload,\n' +
     'Shake or press menu button for dev menu',
 });
+let customData = [{text: " block "}]
+for (let i = 0; i < 10000; i++) {
+  customData.push({text: " block "+i})
+}
+const randomNumber = (min: number, max: number) =>  Math.random() * (max - min) + min
 
+let ref: any
+let setter: any
+const updatePosition = () => {
+  if (ref) {
+    const index = Math.round(randomNumber(0, 300))
+    setter({index})
+    ref.scrollToOffset({offset: (index * 15) - 80})
+  }
+}
+setInterval(updatePosition, 3000)
 export default class App extends React.Component<any, any> {
+  protected state = {index: 0}
+  constructor(props: any) {
+    super(props)
+    setter = (newState: any) => {
+      this.setState(newState)
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <SharedImage view={Image} />
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Scrolled to {this.state.index}
         </Text>
+        <FlatList
+          ref={(newRef) => ref = newRef}
+          style={{maxHeight: 300}}
+          data={customData}
+          keyExtractor={(item) => item.text}
+          renderItem={({item}) => {
+            return <Text style={{lineHeight: 15, fontSize: 15}}>{item.text}</Text>
+          }}
+        />
         <Text style={styles.instructions}>
           To get started, edit App.js
         </Text>
