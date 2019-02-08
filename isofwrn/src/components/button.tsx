a24c2a2d0afd62914fbaf84ebd06ca2bd99d501a
MMB2 <git@mmb2.click>
2019-02-08 20:00:45 +0100

    :page_facing_up: allow easy links with button

--- isofwrn/src/components/button.tsx
+++ isofwrn/src/components/button.tsx
@@ -1,10 +1,21 @@
+import navigatorRefHolder from "isofwrn/src/components/globalNavigator"
 import * as React from "react"
 import { Button, ButtonProps } from "react-native-elements"
 
-const NativeButton: React.FunctionComponent<ButtonProps> = (props) => {
+export interface ICustomNativeButton {
+  href?: any
+}
+
+const NativeButton: React.FunctionComponent<ButtonProps & ICustomNativeButton> = (props) => {
+  const buttonProps = {...props}
+  if (props.href != null) {
+    buttonProps.onPress = () => {
+      navigatorRefHolder.ref.navigate(props.href)
+    }
+  }
   return (
     <Button
-      {...props}
+      {...buttonProps}
     />
   )
 }
