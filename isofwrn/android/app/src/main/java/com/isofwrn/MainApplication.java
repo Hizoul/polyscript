072ee37a0ed564aff33eadfceaa4aab7098f63d4
MMB2 <git@mmb2.click>
2019-03-07 14:21:44 +0100

    :wrench: add react-native-tcp

--- isofwrn/android/app/src/main/java/com/isofwrn/MainApplication.java
+++ isofwrn/android/app/src/main/java/com/isofwrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.isofwrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.peel.react.TcpSocketsModule;
 import com.tradle.react.UdpSocketsModule;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new TcpSocketsModule(),
             new UdpSocketsModule(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
