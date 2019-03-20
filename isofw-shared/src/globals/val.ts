3e9a7be2bf5d4d6e57fbdbe6c759de023a451ae2
Matthias Müller-Brockhausen <git@mmb2.click>
2019-03-20 14:56:27 +0100

    :bug: forgot to add keys required for compilation

--- isofw-shared/src/globals/val.ts
+++ isofw-shared/src/globals/val.ts
@@ -24,10 +24,12 @@ const val = {
     tcp: 1,
     udp: 2,
     packetDelimiter: "_MSG_END_",
-    addServerTimeInfo: false,
-    addServerTimeInfoForWebSockets: false,
+    addServerTimeInfo: true,
+    addServerTimeInfoForWebSockets: true,
     benchmarkEnabled: true,
-    networkToUse: 0
+    networkToUse: 0,
+    useCompression: false,
+    useGzipCompression: false
   },
   // can be set to an object with {url: string, params: any, navigated: false} so rn will go to the page after login
   navigateTo: {
